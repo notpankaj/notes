d79a6a95cd6c525efccc8c85addfbb2f175b9e2b
notpankaj <61045101+notpankaj@users.noreply.github.com>
2025-03-19 01:14:47 +0530

    Update 01-CURRING.js

--- interview/js/practice/01-Curring.js
+++ interview/js/practice/01-Curring.js
@@ -1,1 +1,27 @@
+const sumTwo = (a) => {
+    return (b) => {
+        return a + b
+    }
+}
+const result = sumTwo(5)(10)
+console.log(result)
 
+
+
+
+// multi-chain or n number of curring
+var count = 0
+function sum(a) {
+   count = a
+    //   returning function which return itself
+    function inner (b){
+        if(typeof b === 'number'){
+            count+=b
+        }
+        return inner
+    }
+    return inner
+}
+
+console.log(sum(1)(2)(3)(4)(10)()(-5));
+console.log("count:",count)
